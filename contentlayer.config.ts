dd217b22d42a9aadae803699e72653bce4f9609c
cyj <cyd5538@naver.com>
2023-08-05 16:15:04 +0900

    Feat : contentlayer fields 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -6,6 +6,8 @@ export const Post = defineDocumentType(() => ({
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
+    description : {type : 'string'},
+    tags : {type : "list", required: true, of: { type: 'string' }}
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/posts/${post._raw.flattenedPath}` },
