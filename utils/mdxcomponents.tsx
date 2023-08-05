5b21ade8a385b0678123929ee2e10bdd37c746ba
cyj <cyd5538@naver.com>
2023-08-05 23:09:58 +0900

    Comment : mdxcomponents.tsx 파일 설명

--- utils/mdxcomponents.tsx
+++ utils/mdxcomponents.tsx
@@ -1,8 +1,13 @@
 import React, { PropsWithChildren } from "react";
 
+// MDX 파일에 주입하여 해당 파일에서 컴포넌트를 사용할 수 있게 해줌.
 
-const MDXComponents = {
-  
-};
+const Button = (props: PropsWithChildren) => (
+  <button {...props} style={{backgroundColor:"blue",borderRadius:"5px",color:"white",padding:"2px 10px"}}  >{props.children}</button>
+ );
+ 
+ const MDXComponents = {
+   Button,
+ };
 
 export default MDXComponents;
