aff1388151362dc12e6a249c9c5b902735ee71b2
cyj <cyd5538@naver.com>
2023-08-06 20:59:10 +0900

    Feat : rehype-slug github-slugger 사용해서 TOC 구현

--- app/posts/[slug]/[slugs]/page.tsx
+++ app/posts/[slug]/[slugs]/page.tsx
@@ -30,17 +30,32 @@ const PostLayout = ({ params }: Props) => {
 
   return (
     <div className="flex justify-center">
-      <div className=" w-full h-fit flex flex-col pb-32 ">
+      <div className=" w-full h-fit flex flex-col pb-32">
         <div className="flex flex-col items-center p-2">
-          <p className="text-slate-500  dark:text-white mb-4 mt-24">
+          <p className="text-slate-500  dark:text-white mb-4">
             {format(parseISO(post.date), "LLLL d, yyyy")}
           </p>
           <h1 className="text-slate-900 dark:text-white text-center md:text-5xl/snug text-4xl/snug font-bold">
             {post.title}
           </h1>
-          <article className="w-[350px] min-[500px]:w-full max-w-none prose dark:prose-invert">
-            <MDXContent components={{ ...MDXComponents }} />
+          <article className="w-[350px] min-[500px]:w-full  max-w-none prose dark:prose-invert">
+          <MDXContent components={{ ...MDXComponents }} />
           </article>
+        </div>
+      </div>
+      <div className="w-48">
+        <div className="fixed top-60 right-60 round-md p-4 bg-slate-100 shadow-sm">
+        <h3>TOC</h3>
+        {post.headings.map(heading => {
+            const link = params.slugs + "#" + heading.slug
+            return (
+              <div key={`#${heading.slug}`}>
+                <a className="data-[level=two]:pl-2 data-[level=three]:pl-4" data-level={heading.level} href={link}>
+                  {heading.text}
+                </a>
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,8 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 import rehypePrettyCode from "rehype-pretty-code";
 import remarkGfm from 'remark-gfm';
+import rehypeSlug from "rehype-slug";
+import GithubSlugger from "github-slugger"
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -15,6 +17,27 @@ export const Post = defineDocumentType(() => ({
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/posts/${post._raw.flattenedPath}` },
+    headings: {
+      type: "json",
+      resolve: async (doc) => {
+        const regXHeader = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+        const slugger = new GithubSlugger()
+        const headings = Array.from(doc.body.raw.matchAll(regXHeader)).map(
+            ({ groups }) => {
+              const flag = groups?.flag;
+              const content = groups?.content;
+              return {
+                level: flag?.length == 1 ? "one"
+                : flag?.length == 2 ? "two"
+                : "three",
+                text: content,
+                slug: content ? slugger.slug(content) : undefined
+              };
+            }
+          );
+          return headings;
+        },
+    }
   },
 }))
 
@@ -23,8 +46,9 @@ export default makeSource({
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [[
+    rehypePlugins: [
+      rehypeSlug,
       rehypePrettyCode, 
-    ]],
+    ],
   },
 });
