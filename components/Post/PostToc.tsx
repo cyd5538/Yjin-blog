1f9c1b7fd18152692dc57e666aa8c2038a396415
cyj <cyd5538@naver.com>
2023-08-06 23:51:06 +0900

    Feat : TOC에서 현재부분이 어디인지 보여주는 기능 추가

--- components/Post/PostToc.tsx
+++ components/Post/PostToc.tsx
@@ -1,31 +1,68 @@
-type PostTocType = {
-  toc : {
-    level : string
-    text : string
-    slug : string
-  }[]
-  slugs : string
-}
+"use client"
 
-const PostToc = ({toc,slugs}:PostTocType) => {
+import { useEffect, useRef, useState } from "react";
+
+type PostTocType = {
+  toc: {
+    level: string;
+    text: string;
+    slug: string;
+  }[];
+  slugs: string;
+};
+
+const PostToc = ({ toc, slugs }: PostTocType) => {
+  const [currentHeading, setCurrentHeading] = useState<string>(""); 
+
+  useEffect(() => {
+    const headingsRef = toc.map((heading) => heading.slug);
+
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+
+      for (let i = headingsRef.length - 1; i >= 0; i--) {
+        const headingSlug = headingsRef[i];
+        const headingElement = document.getElementById(headingSlug);
+
+        if (headingElement && headingElement.offsetTop <= scrollY) {
+          setCurrentHeading(headingSlug);
+          break;
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [toc]); 
+
+  console.log(currentHeading)
 
   return (
-    <div className="fixed top-48 w-[250px] rounded-2xl p-4 bg-zinc-300 drop-shadow-md dark:bg-slate-800 dark:text-white border-gray-200">
-      <h3 className="font-bold pb-4">TOC</h3>
+    <div className="fixed top-48 w-[250px] rounded-2xl p-4 bg-purple-400 drop-shadow-md dark:bg-slate-800 dark:text-white border-gray-200">
+      <h3 className="font-bold pb-4">목차</h3>
       <ul className="flex flex-end w-full flex-col">
-        {toc.map(heading => {
-            const link = slugs + "#" + heading.slug
-            return (
-              <li key={`#${heading.slug}`}>
-                <a className="data-[level=two]:pl-2 data-[level=three]:pl-4" data-level={heading.level} href={link}>
-                  {heading.text}
-                </a>
-              </li>
-            )
-          })}
+        {toc.map((heading) => {
+          const link = slugs + "#" + heading.slug;
+          return (
+            <li key={`#${heading.slug}`}>
+              <a
+                className={`${
+                  heading.slug === currentHeading ? "font-bold text-purple-900" : ""
+                } data-[level=two]:pl-2 data-[level=three]:pl-4 text-sm`}
+                data-level={heading.level}
+                href={link}
+              >
+                {heading.text}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default PostToc
+export default PostToc;
