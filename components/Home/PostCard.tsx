2192e6ddca28d901f2e8a55451a97342760ebd63
cyj <cyd5538@naver.com>
2023-08-08 00:56:39 +0900

    Feat : postcard 링크  위치 수정

--- components/Home/PostCard.tsx
+++ components/Home/PostCard.tsx
@@ -6,20 +6,29 @@ import Image from 'next/image';
 function PostCard(post: Post) {
 
   return (
-    <div className="mb-4 rounded-xl shadow-md p-2 cursor-pointer hover:-translate-y-1 ease-in duration-100 bg-white dark:bg-zinc-900">
-    <Link href={post.url}>
+    <div className="mb-4 rounded-xl shadow-md p-2 bg-white dark:bg-zinc-900">
       <div className='flex gap-2 h-40'>
         <div className='mb-4 w-3/4 flex flex-col'>
-          <h2 className="mb-1 mt-3 text-lg pl-1 font-bold ">
-            {post.title}
-          </h2>
+          <Link href={post.url}>
+            <h2 className="mb-1 mt-3 text-3xl pl-1 font-bold cursor-pointer hover:underline">
+              {post.title}
+            </h2>
+          </Link>
           <h3 className="mb-2 text-xs text-gray-400 pl-1">
             {post.description}
           </h3>
           <ul className='flex mt-2 gap-2 pl-1 flex-wrap'>
-            {post.tags?.map((tag,index) => {
-              return <li key={index} className='rounded-md bg-slate-100 dark:bg-zinc-700 pl-1 pr-1'>{tag}</li>
-            })}
+            {post.tags?.map((tag,index) => 
+              <Link 
+                key={index}                       
+                href={{
+                  pathname: `/tag`,
+                  query: { tag: `${tag}` }
+                }}
+              >
+                <li className='rounded-md bg-slate-100 dark:bg-zinc-700 pl-1 pr-1'>{tag}</li>
+              </Link>
+            )}
           </ul>
           <div className='mb-1 mt-4 pl-2'>
             <time dateTime={post.date} className="block text-xs text-gray-600 datk:text-gray-200">
@@ -37,7 +46,6 @@ function PostCard(post: Post) {
           />
         </div>
       </div>
-    </Link>
   </div>
   );
 }
