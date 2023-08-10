ba21e75e71a9ef3b7749d5d9592105d206e3253c
cyj <cyd5538@naver.com>
2023-08-11 00:01:52 +0900

    Feat : tag페이지 메모카테고리 추가

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -9,10 +9,12 @@ import TagCount from './TagCount';
 import usePagination from '@/hooks/usePagination';
 import Title from '../etc/Title';
 import SubTitle from '../etc/SubTitle';
+import Memopost from '../memos/Memopost';
 
 const Tag = () => {
   const [tagCounts, setTagCounts] = useState<{ tag: string; count: number }[]>([]);
   const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+  const [filteredMemo, setFilteredMemo] = useState<Post[]>([]);
 
   const tagParams = useSearchParams();
   const search = tagParams.get('tag');
@@ -20,11 +22,19 @@ const Tag = () => {
   const { currentPage, currentData, totalPages, handlePageChange } = usePagination(filteredPosts);
 
   useEffect(() => {
-    const filtered = allPosts
+    const filteredPost = allPosts
+      .filter(post => !post._id.startsWith('memo'))
       .filter((post) => post.tags?.includes(search as string))
-      .sort((a,b) => compareDesc(new Date(a.date), new Date(b.date)))
-    
-    setFilteredPosts(filtered);
+      .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+
+    setFilteredPosts(filteredPost);
+
+    const filteredMemo = allPosts
+      .filter(post => post._id.startsWith('memo'))
+      .filter((post) => post.tags?.includes(search as string))
+      .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+
+    setFilteredMemo(filteredMemo);
   }, [search]);
 
   useEffect(() => {
@@ -35,7 +45,7 @@ const Tag = () => {
       if (tag !== undefined) {
         if (tagCount[tag as string]) {
           tagCount[tag as string]++;
-        } else { 
+        } else {
           tagCount[tag as string] = 1;
         }
       }
@@ -50,31 +60,36 @@ const Tag = () => {
   }, []);
 
   return (
-    <div>
-      <Title title="Tag"/>
-      <SubTitle subtitle='태그 별로 볼 수 있는 페이지입니다'/>
+    <div className='mb-16'>
+      <Title title="Tag" />
+      <SubTitle subtitle='태그 별로 볼 수 있는 페이지입니다' />
       <div className='flex gap-2   flex-wrap text-xl text-white pb-4 font-bold'>
         {tagCounts.map((tagcount) => (
-          <TagCount key={tagcount.tag} tagCounts={tagcount} search={search as string}/>
+          <TagCount key={tagcount.tag} tagCounts={tagcount} search={search as string} />
         ))}
       </div>
-      <TagTitle title={search as string} length={filteredPosts.length as number}/>
+      <TagTitle title={`${search as string} 태그에 대한 블로그`} length={filteredPosts.length as number} />
       <div className='mt-10 grid gap-4 justify-center md:grid-cols-2 grid-cols-1'>
         {currentData.map((post: Post) => (
           <PostCard key={post.title} {...post} />
         ))}
       </div>
-      <div className='flex justify-center pt-8 pb-32'>
-      {Array.from({ length: totalPages }, (_, index) => (
+      <div className='flex justify-center pt-4 pb-4'>
+        {Array.from({ length: totalPages }, (_, index) => (
           <button
             key={index + 1}
             onClick={() => handlePageChange(index + 1)}
-            className={`mx-2 p-1 pl-2 pr-2 rounded-full  ${
-              currentPage === index + 1 ? 'bg-blue-700  text-white dark:bg-zinc-900' : 'bg-white text-blue-700 dark:text-zinc-800 dark:bg-zinc-500'
-            }`}
+            className={`mx-2 p-1 pl-2 pr-2 rounded-full  ${currentPage === index + 1 ? 'bg-blue-700  text-white dark:bg-zinc-900' : 'bg-white text-blue-700 dark:text-zinc-800 dark:bg-zinc-500'
+              }`}
           >
             {index + 1}
           </button>
+        ))}
+      </div>
+      <TagTitle title={`${search as string} 태그에 대한 메모`} length={filteredMemo.length as number} />
+      <div className='mt-10 grid gap-4 justify-center md:grid-cols-3 grid-cols-1'>
+        {filteredMemo.map((post: Post) => (
+          <Memopost key={post.title} {...post} />
         ))}
       </div>
     </div>
--- components/Tag/TagTitle.tsx
+++ components/Tag/TagTitle.tsx
@@ -7,8 +7,8 @@ const TagTitle:React.FC<TagTitleProps> = ({title, length}) => {
   return (
     <>
       {length !== 0 &&
-        <h1 className="text-4xl font-bold text-white flex gap-2">
-          # <p className="underline">{title}</p> <p>({length})</p>
+        <h1 className="text-2xl mt-10 font-bold text-white flex gap-2">
+          # <p>{title}</p> <p>({length})</p>
         </h1>
       } 
     </>
