3195eaa239b29a17fb685256c06dd089b048a091
cyj <cyd5538@naver.com>
2023-08-11 22:36:56 +0900

    Chore : 빌드 slug 페이지 테스트

--- app/memo/[slug]/page.tsx
+++ app/memo/[slug]/page.tsx
@@ -7,44 +7,45 @@ type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export const generateStaticParams = async () =>
-  allPosts.map((post: Post) => ({ slug: post._raw.flattenedPath }));
+// export const generateStaticParams = async () =>
+//   allPosts.map((post: Post) => ({ slug: post._raw.flattenedPath }));
 
-export const generateMetadata = ({ params }: Props): Metadata => {
-  const post =  allPosts.filter((post : Post) => post._raw.flattenedPath.split("/")[1] === params.slug)[0]
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   const post =  allPosts.filter((post : Post) => post._raw.flattenedPath.split("/")[1] === params.slug)[0]
 
-  return { 
-    title: post?.title, 
-    description: post?.description,
-    openGraph: {
-      title: post?.title,
-      description: post?.description,
-      url: '',
-      siteName: 'Yjin Blog' + post?.title,
-      images: [
-        {
-          url: `${post?.image}`,
-          width: 800,
-          height: 600,
-        },
-        {
-          url: `${post?.image}`,
-          width: 1800,
-          height: 1600,
-          alt: post?.title,
-        },
-      ],
-      locale: 'ko-KR',
-      type: 'website',
-    }, 
-  };
-};
+//   return { 
+//     title: post?.title, 
+//     description: post?.description,
+//     openGraph: {
+//       title: post?.title,
+//       description: post?.description,
+//       url: '',
+//       siteName: 'Yjin Blog' + post?.title,
+//       images: [
+//         {
+//           url: `${post?.image}`,
+//           width: 800,
+//           height: 600,
+//         },
+//         {
+//           url: `${post?.image}`,
+//           width: 1800,
+//           height: 1600,
+//           alt: post?.title,
+//         },
+//       ],
+//       locale: 'ko-KR',
+//       type: 'website',
+//     }, 
+//   };
+// };
 
 export default function Home({ params }: Props) {
-
+  console.log(params)
   return (
     <div className='pb-32'>
-      <Memo params={params}/>
+      gdgdgd
+      {/* <Memo params={params}/> */}
     </div>
   )
 }
