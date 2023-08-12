55d790c3017b8aa1673f7be3eb0accb4b0c00cd5
cyj <cyd5538@naver.com>
2023-08-12 21:46:49 +0900

    Test : basePath 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
+  basePath: '/',
   reactStrictMode: true, 
   swcMinify: true, 
   output: 'export',
