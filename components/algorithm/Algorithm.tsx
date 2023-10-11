fb13565d577f639b6450a93fa5a92530993b1424
cyj <cyd5538@naver.com>
2023-10-11 10:39:45 +0900

    Feat: remove description

--- components/algorithm/Algorithm.tsx
+++ components/algorithm/Algorithm.tsx
@@ -8,8 +8,7 @@ const Algorithm:React.FC<AlgorithmProps> = ({algorithm}) => {
   const url = algorithm.url.split("/")[1]
   
   return (
-    <Link href={`/algorithm/${url}`} className="cursor-pointer flex gap-2 pb-1 pt-1 justify-center items-center rounded-md bg-violet-400 hover:bg-violet-500 dark:hover:bg-slate-800 text-white dark:bg-slate-700">
-      <div>{algorithm.description}</div>
+    <Link href={`/algorithm/${url}`} className="cursor-pointer flex pb-1 pt-1 justify-center items-center rounded-md bg-violet-400 hover:bg-violet-500 dark:hover:bg-slate-800 text-white dark:bg-slate-700">
       <div className="font-bold">{algorithm.title}</div>
     </Link>
   )
