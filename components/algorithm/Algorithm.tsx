d94d8acc4184c6f02ce1838ef063489821f66e35
cyj <cyd5538@naver.com>
2023-10-13 16:21:19 +0900

    Feat: algorithm link edit

--- components/algorithm/Algorithm.tsx
+++ components/algorithm/Algorithm.tsx
@@ -5,10 +5,9 @@ interface AlgorithmProps {
   algorithm: Post
 }
 const Algorithm:React.FC<AlgorithmProps> = ({algorithm}) => {
-  const url = algorithm.url.split("/")[1]
-  
+
   return (
-    <Link href={`/algorithm/${url}`} className="cursor-pointer flex pb-1 pt-1 justify-center items-center rounded-md bg-violet-400 hover:bg-violet-500 dark:hover:bg-slate-800 text-white dark:bg-slate-700">
+    <Link href={`/algorithm/${algorithm.url.split("/")[1]}`} className="cursor-pointer flex pb-1 pt-1 justify-center items-center rounded-md bg-violet-400 hover:bg-violet-500 dark:hover:bg-slate-800 text-white dark:bg-slate-700">
       <div className="font-bold">{algorithm.title}</div>
     </Link>
   )
