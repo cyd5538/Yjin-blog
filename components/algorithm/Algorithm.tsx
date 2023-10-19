ef36f535b97fa81e3ed419eb63f8742081134099
cyj <cyd5538@naver.com>
2023-10-19 15:31:31 +0900

    Feat: classname edit
    클래스네임 silver,bronze,gold, lv1,lv2 분리해서 배경색 적용

--- components/algorithm/Algorithm.tsx
+++ components/algorithm/Algorithm.tsx
@@ -5,9 +5,25 @@ interface AlgorithmProps {
   algorithm: Post
 }
 const Algorithm:React.FC<AlgorithmProps> = ({algorithm}) => {
-
+  const title = algorithm.title.replace(/프로그래머스 |백준 /g, '')
+  
+  const ClassName = `${
+    title.startsWith("Silver") ? "bg-[#4E6A86]" :
+    title.startsWith("Gold") ? "bg-[#c6fc16]" :
+    title.startsWith("Bronze") ? "bg-[#9D4900]" :
+    title.startsWith("Lv2") ? "bg-blue-500" :
+    title.startsWith("Lv1") ? "bg-blue-300" :
+    ""
+  }`;
+  
+  const commonClassName = `cursor-pointer flex pb-1 pt-1 justify-center items-center rounded-md text-white `
+  
+  const finalClassName = `${commonClassName} ${ClassName}`;
   return (
-    <Link href={`/algorithm/${algorithm.url}`} className="cursor-pointer flex pb-1 pt-1 justify-center items-center rounded-md bg-violet-400 hover:bg-violet-500 dark:hover:bg-slate-800 text-white dark:bg-slate-700">
+    <Link 
+      href={`/algorithm/${algorithm.url}`} 
+      className={finalClassName}
+    >
       <div className="font-bold">{algorithm.title.replace(/프로그래머스|백준/g, '').replace(/\s+/g, ' ')}</div>
     </Link>
   )
