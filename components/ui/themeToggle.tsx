bcf2164174dc0a31c157926b6fcfacb896b0dfc3
cyj <cyd5538@naver.com>
2023-11-03 13:23:31 +0900

    Fix: theme toggle btn fix
    버튼을 클릭할때는 작동이 안되고 버튼안의 아이콘을 클릭해야 작동이 되는 코드 수정

--- components/ui/themeToggle.tsx
+++ components/ui/themeToggle.tsx
@@ -4,13 +4,25 @@ import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme();
+
+  const toggleTheme = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  }
 
   return (
-      <Button variant="secondary" size="icon">
-        <Sun onClick={() => setTheme("dark")} className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon onClick={() => setTheme("light")} className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
-      </Button>
+    <Button variant="secondary" size="icon" onClick={toggleTheme}>
+      <Sun className={`h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ${theme === 'dark' ? 'dark:-rotate-90 dark:scale-0' : ''}`} />
+      <Moon className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all ${theme === 'dark' ? 'dark:rotate-0 dark:scale-100' : ''}`} />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
+
+
+
+
