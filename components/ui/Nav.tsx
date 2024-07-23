34b7296aade70994d9342ca18c673ec492d5831f
cyd5538 <cyd5538@naver.com>
2024-07-23 16:44:10 +0900

    Design: nav
    nav dark text change

--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -53,12 +53,12 @@ const Nav = () => {
             </SheetTrigger>
             <SheetContent className='dark:bg-zinc-900'>
               <SheetHeader>
-                <SheetTitle className='text-2xl pt-8 font-bold text-white '>YJIN의 블로그입니다.</SheetTitle>
-                <SheetDescription className='pt-10 pb-2 cursor-pointer text-xl text-white hover:underline dark:text-white dark:hover:underline underline-offset-8 dark:hover:text-gray-100 hover:text-gray-300'>
+                <SheetTitle className='text-2xl pt-8 font-bold text-black dark:text-white'>YJIN의 블로그입니다.</SheetTitle>
+                <SheetDescription className='pt-10 pb-2 cursor-pointer text-xl text-black hover:underline dark:text-white dark:hover:underline underline-offset-8 dark:hover:text-gray-100 hover:text-gray-300'>
                   Home
                 </SheetDescription>
                 {Links.map((link) => {
-                  return <SheetDescription key={link.title} className='cursor-pointer text-xl text-white hover:underline dark:text-white dark:hover:underline underline-offset-8 dark:hover:text-gray-100 hover:text-gray-300 pb-2'>
+                  return <SheetDescription key={link.title} className='cursor-pointer text-xl text-black hover:underline dark:text-white dark:hover:underline underline-offset-8 dark:hover:text-gray-100 hover:text-gray-300 pb-2'>
                     <Link href={link.link}>{link.title}</Link>
                   </SheetDescription>
                 })}
